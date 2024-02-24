403b7f77f3af4cf1a52a32778b621df877399f51
Michael-Quon <michaelquon08@gmail.com>
2024-02-24 15:22:57 -0500

    Added the java fragment classes, and put my first name, last name, and studentID as the first comment in each

---
+++ MichaelModuleMid/src/main/java/michael/quon/n01565129/MichaelFragment.java
@@ -0,0 +1,31 @@
+// Michael Quon N01565129
+package michael.quon.n01565129;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MichaelFragment extends Fragment {
+
+    public MichaelFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_michael, container, false);
+    }
+}
---
+++ MichaelModuleMid/src/main/java/michael/quon/n01565129/N01565129Fragment.java
@@ -0,0 +1,30 @@
+// Michael Quon N01565129
+package michael.quon.n01565129;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class N01565129Fragment extends Fragment {
+
+    public N01565129Fragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_n01565129, container, false);
+    }
+}
