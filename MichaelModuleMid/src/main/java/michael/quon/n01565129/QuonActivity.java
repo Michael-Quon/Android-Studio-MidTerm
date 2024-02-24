b557d191b5d35256f78229c93b8c97641b0ae105
Michael-Quon <michaelquon08@gmail.com>
2024-02-24 17:46:09 -0500

    Added the tabs from lab 6 using the ViewPagerAdapter.java, renamed the tabs with using a switch. And added the background colours for the fragments

--- MichaelModuleMid/src/main/java/michael/quon/n01565129/QuonActivity.java
+++ MichaelModuleMid/src/main/java/michael/quon/n01565129/QuonActivity.java
@@ -1,8 +1,17 @@
+// Michael Quon N01565129
 package michael.quon.n01565129;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
 
 public class QuonActivity extends AppCompatActivity {
 
@@ -10,5 +19,34 @@ public class QuonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new MichaelFragment());
+        fragmentList.add(new QuonFragment());
+        fragmentList.add(new N01565129Fragment());
+
+        ViewPager2 viewPager = findViewById(R.id.MichaelviewPager);
+        viewPager.setAdapter(new ViewPagerAdapter(this, fragmentList));
+
+        // Disable swiping
+        viewPager.setUserInputEnabled(false);
+
+        TabLayout tabLayout = findViewById(R.id.MichaeltabLayout);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    // changed the tab name
+                    switch (position) {
+                        case 0:
+                            tab.setText(R.string.michael);
+                            break;
+                        case 1:
+                            tab.setText(R.string.quon);
+                            break;
+                        case 2:
+                            tab.setText(R.string.n01565129);
+                            break;
+                    }
+                }
+        ).attach();
     }
 }
---
+++ MichaelModuleMid/src/main/java/michael/quon/n01565129/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+// Michael Quon N01565129
+package michael.quon.n01565129;
+
+// ViewPagerAdapter.java
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    private final ArrayList<Fragment> fragmentList;
+
+    public ViewPagerAdapter(FragmentActivity fragmentActivity, ArrayList<Fragment> fragmentList) {
+        super(fragmentActivity);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+}
+
