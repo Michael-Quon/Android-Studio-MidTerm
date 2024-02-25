bba5bb7116f20bd0995e0c10923a2011a27c2794
Michael-Quon <michaelquon08@gmail.com>
2024-02-24 23:01:21 -0500

    Made the third fragment (N01565129Fragment) have an imageview and the textclock. Along with a counter to display as a toast when clicking off the fragment that increases every second.

--- MichaelModuleMid/src/main/java/michael/quon/n01565129/N01565129Fragment.java
+++ MichaelModuleMid/src/main/java/michael/quon/n01565129/N01565129Fragment.java
@@ -8,9 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextClock;
+import android.widget.Toast;
 
 
 public class N01565129Fragment extends Fragment {
+
+    private int counter = 0;
 
     public N01565129Fragment() {
         // Required empty public constructor
@@ -18,13 +23,54 @@ public class N01565129Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_n01565129, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_n01565129, container, false);
+
+        ImageView imageView = view.findViewById(R.id.MichaelimageView);
+        TextClock textClock = view.findViewById(R.id.MichaeltextClock);
+
+        return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // start counter when fragment resumes
+        startCounter();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // stop counter when fragment pauses
+        stopCounter();
+    }
+
+    private void startCounter() {
+        new Thread((new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(1000); // every second
+                        counter++;
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        })).start();
+    }
+
+    private void stopCounter() {
+        // stops counter, and displays toast with value of counter and name+id
+        Toast.makeText(requireContext(), getString(R.string.counter) + counter + " " + getString(R.string.michael_quon_id), Toast.LENGTH_LONG).show();
+    }
+
 }
